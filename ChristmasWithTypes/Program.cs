5a47615091e1d93ebe8514f5422141a2b96a25f2
Zoey Baker <zoeybaker625@gmail.com>
2022-05-24 11:15:23 -0500

    initial commit

--- ChristmasWithTypes/Program.cs
+++ ChristmasWithTypes/Program.cs
@@ -11,13 +11,16 @@ namespace ChristmasWithTypes
             var xmasDay = Christmas.Day.Thursday;
 
             //TODO set Santa's name to Kris Kringle
-            xmas.Santa = null;
+            xmas.KrisKringle = null;
 
             //TODO Insert 3 presents you would like for xmas.  They must be strings
-            xmas.Presents =  new string[3]{ }; 
+            xmas.Presents =  new string[3]{Pony};
+            xmas.Presents = new string[3] {Spongebob Candy};
+            xmas.Presents = new string[3] {Money};
+
 
             //TODO Set the TreeHeight to 10
-            xmas.TreeHeight = null;
+            xmas.TreeHeight = 10;
 
             Console.WriteLine($"This year christmas falls on {xmasDay} \n");
             Console.WriteLine($"Our tree will be {xmas.TreeHeight} feet high \n");
